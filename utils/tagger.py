94525d33094370cad7442d81bbf064459f89b347
andre <andrinho@gmail.com>
2025-06-02 05:41:19 -0300

    add tagging utils

---
+++ utils/tagger.py
@@ -0,0 +1,30 @@
+#   util for helping managing resources tagging
+
+#  my common tags are here but should be somewhere else
+common_tags_list=[
+    {
+        'Key': 'project',
+        'Value': 'project_boto3'
+    },
+    {
+        'Key': 'author',
+        'Value': 'vidalme'
+    }
+]
+
+# breaks down all values from a dict into smaller 
+# dicts in aws tag format and returns a list of them
+def tag_formatter(tags:dict)->list:
+    new_list = []
+    for key,value in tags.items():
+        new_list.append(
+            {
+                'Key': key,
+                'Value': value
+            }
+        )
+    return new_list
+
+# merges a common set of tags with the callers requested tags
+def tagit(spec_tags:list)->list:
+    return common_tags_list + spec_tags
