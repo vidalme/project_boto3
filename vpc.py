da65ef995717551df8c9a29b3f23ef14613bf98d
andre <andrinho@gmail.com>
2025-06-02 06:23:37 -0300

    feat: checks if there is already a vpc created

--- vpc.py
+++ vpc.py
@@ -9,6 +9,19 @@ from utils.tagger import tagit
 def create_vpc( client:boto3.client , vpc_cidr:str , env:str ):
     # VPC creation and tagging
     try:
+        
+        describe_response = client.describe_vpcs(
+            Filters=[
+                {
+                    'Name': 'tag:Name',
+                    'Values': ['vpc_project_boto3_'+env]
+                },
+            ],
+        )
+        if len(describe_response['Vpcs']) != 0:
+            print("This VPC here already existed so I will not create a new one - > "+describe_response['Vpcs'][-1]['VpcId'])
+            return describe_response['Vpcs'][-1]['VpcId']
+
         vpc_response = client.create_vpc(
             CidrBlock = vpc_cidr,
             TagSpecifications=[
