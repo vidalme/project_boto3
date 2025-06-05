e2fbfaceb06e07c200375d3e230ff0aa2b099800
andre <andrinho@gmail.com>
2025-06-05 04:29:21 -0300

    add region definition parameter for boto3,client creation

--- application_stack_provision.py
+++ application_stack_provision.py
@@ -15,8 +15,8 @@ vpc_cidr = os.getenv('VPC_CIDR')
 subnet_cidr = os.getenv('SUBNET_CIDR')
 env = os.getenv('ENV')
 
-ec2_client = boto3.client('ec2')
+ec2_client = boto3.client('ec2',region_name=region)
 
 if __name__ == '__main__':
-    vpc_id = create_vpc( ec2_client , vpc_cidr )
-    create_subnets(ec2_client,vpc_id)
+    vpc_id = create_vpc(ec2_client, vpc_cidr)
+    create_subnets(ec2_client, vpc_id)
