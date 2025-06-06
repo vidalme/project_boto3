6d703c1b1611803d3beed9065fadec16521d7aaf
andre <andrinho@gmail.com>
2025-06-06 05:34:18 -0300

    change order for resources removal

--- application_stack_destroy.py
+++ application_stack_destroy.py
@@ -5,8 +5,9 @@ import boto3
 from dotenv import load_dotenv
 load_dotenv()
 
-from internet_gateway import destroy_internet_gateway
+from route_table import destroy_route_table
 from subnet import destroy_all_subnets
+from internet_gateway import destroy_internet_gateway
 from vpc import destroy_all_non_default_vpcs
 
 project = os.getenv('PROJECT')
@@ -14,6 +15,7 @@ project = os.getenv('PROJECT')
 client = boto3.client('ec2')
 
 if __name__ == '__main__':
-    destroy_internet_gateway(client)
+    destroy_route_table(client)
     destroy_all_subnets(client)
+    destroy_internet_gateway(client)
     destroy_all_non_default_vpcs(client)
